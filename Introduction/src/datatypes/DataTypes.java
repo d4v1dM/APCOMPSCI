f18f9534e70c13d0c3473eed0640a2865549dac8
Student 6 <Student 6@10.8.33.159>
2016-09-19 13:16:53 -0400

    Added package for datatypes

---
+++ Introduction/src/datatypes/DataTypes.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class DataTypes {
+
+
+	public static void main(String[] args){
+		System.out.println(5.0/2);
+		System.out.println((double) 5.0/2); // data type will be casted.
+		System.out.println(5/2);
+		// because 5 and 2 are both integers, so the return type will be integer.
+		
+		System.out.println(3.0+5/2+5*2); //java takes data type that takes most space
+	}
+
+
+}
