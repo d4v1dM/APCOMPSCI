3eb552e4647f15c0770b57a821c523a3dbde706e
d4v1dM <dmedina5861@bths.edu>
2016-09-19 23:05:14 -0400

    Comment added

--- Introduction/src/datatypes/DataTypes.java
+++ Introduction/src/datatypes/DataTypes.java
@@ -8,7 +8,7 @@ public class DataTypes {
 		System.out.println((double) 5.0/2); // data type will be casted.
 		System.out.println(5/2);
 		// because 5 and 2 are both integers, so the return type will be integer.
-		
+		// added to check if github is working
 		System.out.println(3.0+5/2+5*2); //java takes data type that takes most space
 	}
 
