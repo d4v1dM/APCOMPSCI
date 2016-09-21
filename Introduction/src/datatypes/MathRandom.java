9ed513ee98f62b1e5744b4e438b9c8efddd3cce9
Student 6 <Student 6@10.8.33.159>
2016-09-21 12:39:53 -0400

    Added random dices

--- Introduction/src/datatypes/MathRandom.java
+++ Introduction/src/datatypes/MathRandom.java
@@ -5,17 +5,37 @@ import java.util.Random;
 public class MathRandom {
 
 	public static void main(String[] args){
+		int[] results = new int[6];
 		for(int index = 0; index < 10; index++){
 			System.out.println("Roll #" + (index + 1) + " Die is " + rollFairDie());
+			
+		}
+		for(int i = 0; i < 6;i++){
+			System.out.println( (i + 1) + " was rolled " + results[i] + " this many times " + results[i]);
 		}
 	}
 	public static int rollFairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6) + 1;
+//		Random gen = new Random();
+//		return gen.nextInt(6) + 1;
+		
+		double rand = Math.random();
+		int roll = (int) (rand * 6); // [0,5] what above code does.
+		return roll + 1;
 	}
 	public static int rollUnfairDie(){
-		Random gen = new Random();
-		return gen.nextInt(6) + 1;
+		int rand = rollFairDie();
+		if(rand % 2 == 0 && rand < 6){
+			return rand + 1;
+		}else{
+			if(rand > 0){
+				return rand -1;
+			}
+		}
+		if(rand == 1){
+			return 1;
+		}
+		return rand;
+		
 	}
 
 }
