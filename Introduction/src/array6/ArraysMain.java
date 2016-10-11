85cb336a47ea8fc5e2a28b1a56643c611149bb5e
Student 6 <Student 6@10.8.33.159>
2016-10-11 13:07:57 -0400

    Added stuff.

--- Introduction/src/array6/ArraysMain.java
+++ Introduction/src/array6/ArraysMain.java
@@ -9,6 +9,22 @@ public class ArraysMain {
 		//primitive type arrays fill with ZEROS.
 		int[] zeros1 = {0,0,0};
 		int[] zeros2 = new int[3];
+		boolean[] booleans = new boolean[3];
+		// iterate two ways.
+		// you use this for loop when the index of data is important to reference.
+		// you need to customize how you iterate.
+		// (increase by 2, backwards, etc...)
+		for(int i = 0; i < booleans.length; i++){
+			System.out.println(i + ") " + booleans[i]);
+		}
+		
+		//second method: for each
+		//always goes in order, does not keep track of index
+		//easier to type;
+		
+		for(boolean b: booleans){
+			System.out.println("" + b);
+		}
 		
 		String[] strings = {"","",""};
 		String[] strings2 = new String[3];
