7578fcfdac7fec47069a36248d063ac4d60886a1
Student 6 <Student 6@10.8.33.159>
2016-10-11 13:17:09 -0400

    Added string and timed.

--- Introduction/src/array6/ArraysMain.java
+++ Introduction/src/array6/ArraysMain.java
@@ -3,12 +3,16 @@ package array6;
 public class ArraysMain {
 
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		arrayIntroMethod();
+		long endTime = System.currentTimeMillis();
+		System.out.println("Completed method in " + (endTime - startTime));
 		
-		// construct 2 integer arrays
-		// these two methods do the same thing.
-		//primitive type arrays fill with ZEROS.
-		int[] zeros1 = {0,0,0};
-		int[] zeros2 = new int[3];
+	}
+	
+	public static void arrayIntroMethod(){
+//		int[] zeros1 = {0,0,0};
+//		int[] zeros2 = new int[3];
 		boolean[] booleans = new boolean[3];
 		// iterate two ways.
 		// you use this for loop when the index of data is important to reference.
@@ -28,6 +32,13 @@ public class ArraysMain {
 		
 		String[] strings = {"","",""};
 		String[] strings2 = new String[3];
+		
+		for(String s: strings){
+			System.out.println("" + s);
+		}
+		for(String s: strings2){
+			System.out.println("" + s);
+		}
 	}
 
 }
