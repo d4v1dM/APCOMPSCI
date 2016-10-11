221753f2bfe3a0de25d4236a1495fe3255865747
Student 6 <Student 6@10.8.33.159>
2016-10-11 12:58:37 -0400

    Added Array practice.

--- Introduction/src/array6/ArraysMain.java
+++ Introduction/src/array6/ArraysMain.java
@@ -3,8 +3,15 @@ package array6;
 public class ArraysMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		// construct 2 integer arrays
+		// these two methods do the same thing.
+		//primitive type arrays fill with ZEROS.
+		int[] zeros1 = {0,0,0};
+		int[] zeros2 = new int[3];
+		
+		String[] strings = {"","",""};
+		String[] strings2 = new String[3];
 	}
 
 }
