cb0f5cbf078070ffa79d81e844cc3270ee6f0df0
Student 6 <Student 6@10.8.33.159>
2016-10-14 12:31:05 -0400

    Sample class has been added.

---
+++ Introduction/src/array6/SampleElement.java
@@ -0,0 +1,19 @@
+package array6;
+
+public class SampleElement {
+	//field
+	private int number;
+	
+	//constructor
+	public SampleElement(int value){
+		this.number = value;
+	}
+	
+	public int getNumber(){
+		return this.number;
+	}
+	
+	public void increase(){
+		this.number++;
+	}
+}
