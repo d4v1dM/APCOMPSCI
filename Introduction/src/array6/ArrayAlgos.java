19b56547e39201827eb3358c910104e7d265d03f
Student 6 <Student 6@10.8.33.159>
2016-10-21 13:09:06 -0400

    More algooos

--- Introduction/src/array6/ArrayAlgos.java
+++ Introduction/src/array6/ArrayAlgos.java
@@ -98,12 +98,16 @@ public class ArrayAlgos {
 	public static double[] selectSort(double[] arr){
 		int select = -1;
 		for(int i = 0; i < (arr.length - 1); ++i){
+			System.out.println("current: " + arr[i]);
 			for(int j = (i+1); j < arr.length; ++j){
 				if(arr[j] < arr[i]){
 					select = j;
 				}
 			}
 			if(select != -1) swap(arr,i,select);
+			for(double s: arr){
+				System.out.println(s);
+			}
 		}
 		return arr;
 	}
