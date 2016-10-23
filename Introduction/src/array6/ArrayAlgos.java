8eec11f863fb23f22fad8c5baab674744ad44019
david <dmedina5861@bths.edu>
2016-10-22 21:17:05 -0400

    started consecutive.

--- Introduction/src/array6/ArrayAlgos.java
+++ Introduction/src/array6/ArrayAlgos.java
@@ -146,4 +146,27 @@ public class ArrayAlgos {
 		}
          return ctr;
     }
+	public static int longestConsecutiveSequence(int[] array1){
+        /**This method counts the longest consecutive sequence in an array.
+         * It does not matter where the sequence begins
+         * If there are no consecutive numbers, the method should return '1'
+         * 
+         * Examples:
+         * longestSequence({1,2,3,4,5,8,9}) returns '5', since the sequence '1,2,3,4,5' is 5 integers long 
+         * longestSequence({0,9,10,11,4,3,8,9}) returns '3', since '9,10,11' is 3 integers long
+         * longestSequence({0,9,8,11,4,3,7,9}) returns '1', since there are no consecutive integers
+         * */
+		int[] longst = new int[array1.length];
+		int ctr = 0;
+		for(int i = 0; i < (array1.length - 1); ++i){
+			if(array1[i] == (array1[i] + 1)){
+				++longst[ctr];
+			} else{
+				++ctr;
+				
+			}
+		}
+        
+        return 0;
+    }
 }
