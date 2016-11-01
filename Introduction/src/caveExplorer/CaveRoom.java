6af25ab4c35a2abad0626d57c68b1f680077df4f
Student 6 <Student 6@10.8.33.159>
2016-11-01 12:03:44 -0400

    Added stuff for project.

--- Introduction/src/caveExplorer/CaveRoom.java
+++ Introduction/src/caveExplorer/CaveRoom.java
@@ -2,4 +2,27 @@ package caveExplorer;
 
 public class CaveRoom {
 
+	public CaveRoom(String description) {
+		// TODO Auto-generated constructor stub
+		
+	}
+	// "final" never changes.
+	public static final int NORTH = 0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEST = 3;
+	
+	private String description;
+	private String contents;
+	private String defaultContents;
+	private String directions;
+	
+	private CaveRoom[] borderingRooms;
+	private Door[] doors;
+	
+	
+	public void enter() {
+		// TODO Auto-generated method stub
+		// sets up cave for when entered by player.
+	}
 }
---
+++ Introduction/src/caveExplorer/Door.java
@@ -0,0 +1,9 @@
+package caveExplorer;
+
+public class Door {
+
+	private boolean locked;
+	private boolean open;
+	
+	
+}
