afd0c3ab32f8bc2b0391830bb86faffe0ebeb7d1
Student 6 <Student 6@10.8.33.159>
2016-11-02 12:02:28 -0400

    added print array procedure.

--- Introduction/src/caveExplorer/TwoDArrayIntro.java
+++ Introduction/src/caveExplorer/TwoDArrayIntro.java
@@ -8,21 +8,33 @@ public class TwoDArrayIntro {
 		// TODO Auto-generated method stub
 
 		int[] arr = {0,1,2,3,4,5,6,7};
-		System.out.println(Arrays.toString(arr));
+		//System.out.println(Arrays.toString(arr));
 		
 		// rows, columns -> (y,x)
 		String[][] arr2D = new String[4][3];
 		
-		//iterate row by row
-		for(int row = 0; row < arr2D.length; ++row){
-			for(int col = 0; col < arr2D[row].length; ++col){
-				arr2D[row][col] = "(" + row + "," + col + ")";
+//		//iterate row by row
+//		for(int row = 0; row < arr2D.length; ++row){
+//			for(int col = 0; col < arr2D[row].length; ++col){
+//				arr2D[row][col] = "(" + row + "," + col + ")";
+//			}
+//		}
+//		// print each row
+//		for(String[] row: arr2D){
+//			System.out.println(Arrays.toString(row));
+//		}
+		
+		print2D(arr2D);
+	}
+
+	public static void print2D(String[][] arr){
+		for(String[] strArr: arr){
+			for(String str: strArr){
+				System.out.println(str);
 			}
-		}
-		// print each row
-		for(String[] row: arr2D){
-			System.out.println(Arrays.toString(row));
 		}
 	}
 
+
 }
+
