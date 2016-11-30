4b91e6f2f15429d880d1ebbb0b677fff7f5d4d24
Student 6 <Student 6@10.8.33.159>
2016-11-30 12:25:02 -0500

    Insertion sort.

---
+++ Introduction/src/sort/SortAlgos.java
@@ -0,0 +1,17 @@
+package sort;
+
+public class SortAlgos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void insertion_sort(int[] arr){
+		for(int i = 0; i < arr.length; ++i){
+			for(int j = (i + 1); j > 0; ++j){
+				
+			}
+		}
+	}
+
+}
