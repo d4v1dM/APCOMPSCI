b39969185aeabe620602b21865540ad922270d62
Student 6 <Student 6@10.8.33.159>
2016-11-30 12:33:26 -0500

    Stuff Stuff

--- Introduction/src/sort/SortAlgos.java
+++ Introduction/src/sort/SortAlgos.java
@@ -7,9 +7,11 @@ public class SortAlgos {
 
 	}
 	public static void insertion_sort(int[] arr){
+		int min = 0; // malloc.
 		for(int i = 0; i < arr.length; ++i){
+			min = i + 1; // initial min.
 			for(int j = (i + 1); j > 0; ++j){
-				
+				if(arr[j] < arr[min]) min = j; // save index of new min.
 			}
 		}
 	}
