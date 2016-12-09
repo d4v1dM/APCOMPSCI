90eb586f74f8654f1e16b9573e2ad42f80845f34
Student 6 <Student 6@10.8.33.159>
2016-12-09 12:03:17 -0500

    ghgghgvghgh

---
+++ Introduction/src/GUI/GUI_Application.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class GUI_Application extends JFrame{
+	
+	public GUI_Application(){
+		
+	}
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		
+	}
+
+}
