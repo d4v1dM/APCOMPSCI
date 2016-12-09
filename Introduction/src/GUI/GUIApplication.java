daa3db7d85bab141a8e2551174f7ae81a96f7736
Student 6 <Student 6@10.8.33.159>
2016-12-09 12:30:50 -0500

    dgsdgsg

--- Introduction/src/GUI/GUIApplication.java
+++ Introduction/src/GUI/GUIApplication.java
@@ -1,16 +1,27 @@
 package GUI;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame{
-	
+	private Screen currentScreen;
 	public GUIApplication(int width, int height){
 		//constructor
 		super();
 		setBounds(20,20,width,height);
 		// terminates program when window is closed.
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		initScreen();
 		setVisible(true);
+	}
+
+	private void initScreen() {
+		// TODO Auto-generated method stub
+		currentScreen = new Screen(getWidth(), getHeight());
+	}
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
 	}
 
 	// demo purposes only.
--- Introduction/src/GUI/Screen.java
+++ Introduction/src/GUI/Screen.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class Screen {
@@ -12,12 +13,20 @@ public class Screen {
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB_PRE);
 		update();
 	}
+	private void initImage(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
 	public BufferedImage getImage(){
 		return this.image;
 	}
 	public void update(){
 		Graphics2D g = image.createGraphics();
+		// smooth the graphics
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.pink);
 		g.fillRect(0,0,image.getWidth(), image.getHeight());
+		g.setColor(Color.black);
+		g.drawString("Hello World", 40, 100);
 	}
 }
