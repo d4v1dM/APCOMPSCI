15b88e0c87473e2ac3cbd8776d6a179c2a5ade3e
Student 6 <Student 6@10.8.33.159>
2016-12-15 11:56:47 -0500

    sdgfsdgsd

---
+++ Introduction/src/array6/BoolArray.java
@@ -0,0 +1,16 @@
+package array6;
+
+public class BoolArray {
+
+	public static void main(String[] args){
+		boolean[] arr = {true, false, true, true, false};
+		printArr(arr);
+	}
+	public static void printArr(boolean[] arr){
+		for(boolean a: arr){
+			if(a) System.out.println("O");
+			else System.out.println("X");
+		}
+	}
+
+}
