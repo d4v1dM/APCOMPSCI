4ebac967f0fa05953bce9c66bec9683443fa5a94
Student 6 <Student 6@10.8.33.159>
2016-12-15 12:17:12 -0500

    156156456

--- Introduction/src/array6/BoolArray.java
+++ Introduction/src/array6/BoolArray.java
@@ -1,14 +1,36 @@
 package array6;
 
 public class BoolArray {
+	private static boolean[][] grid;
 
 	public static void main(String[] args){
-		boolean[][] arr;
-		arr = new boolean[5][5];
-		printArr(arr);
+		grid = new boolean[5][5];
+		printArr(grid);
+		buttonClicked(3,3);
 	}
-	public static void printArray(boolean[] arr){
+	public static void switchRow(boolean[] row, int x){
 		
+	}
+	public static void switchColumn(boolean[][] grid, int r, int c){
+		
+	}
+	public static void buttonClicked(int r, int c) {
+		// TODO Auto-generated method stub
+		if(r >= 0 && r < grid.length){
+			switchRow(grid[r], c);
+		}
+		switchColumn(grid, r, c);
+		// check if any lights are on.
+		boolean win = true;
+		for(int i = 0; i < grid.length; ++i){
+			for(int j = 0; j < grid[i].length; ++j){
+				if(grid[i][j]){
+					win = false;
+					break;
+				}
+			}
+		}
+		if(win) System.out.println("You have won.");
 	}
 	public static void printArr(boolean[][] arr){
 		for(boolean[] a: arr){
@@ -18,5 +40,6 @@ public class BoolArray {
 			}
 			System.out.println("");
 		}
+		System.out.println("-----");
 	}
 }
